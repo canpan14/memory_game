6f095c02680328db8a8838b9f21a81e2547ddb17
canpan14 <sammachlin@hotmail.com>
2017-08-12 19:24:39 -0400

    Unit 10 - Changed cards to objects
    Also adjusted methods as needed.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "image/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "image/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "image/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "image/king-of-diamonds.png"
+}];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-		if(cardsInPlay[0] === cardsInPlay[1]){
-			console.log("You found a match!");
-		} else {
-			console.log("Sorry, try again.");
-		}
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("Suit was " + cards[cardId].suit);
+	console.log("Suit was " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 		cardsInPlay.length = 0;
