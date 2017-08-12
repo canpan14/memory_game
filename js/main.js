ce18fe1b2b72fe4e58ff93787b59fc815f90522b
canpan14 <sammachlin@hotmail.com>
2017-08-12 16:50:56 -0400

    Unit 8 javascript code added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+} else {
+	// Wait for two cards to be flipped
+}
