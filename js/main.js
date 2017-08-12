1f66c51568b3414ec77feab5a928c0418658fdb0
canpan14 <sammachlin@hotmail.com>
2017-08-12 18:37:12 -0400

    Moved js code into functions - Unit 9

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+var checkForMatch = function() {
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+};
 
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+		cardsInPlay.length = 0;
 	} else {
-		alert("Sorry, try again.");
+		// Wait for two cards to be flipped
 	}
-} else {
-	// Wait for two cards to be flipped
-}
+};
+
+flipCard(0);
+flipCard(1);
