02c49e120e71622da6a77cc7e6b4e61ab8d8b892
canpan14 <sammachlin@hotmail.com>
2017-08-13 14:31:44 -0400

    Unit 11 - Final Code
    Code is a deliverable state. Anything after this is for fun.

--- js/main.js
+++ js/main.js
@@ -2,45 +2,51 @@ var cards = [
 {
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "image/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png"
 },
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "image/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "image/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 },
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "image/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 }];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log("Suit was " + cards[cardId].suit);
-	console.log("Suit was " + cards[cardId].cardImage);
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 		cardsInPlay.length = 0;
-	} else {
-		// Wait for two cards to be flipped
 	}
 };
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++){
+		var cardToAdd = document.createElement("img");
+		cardToAdd.setAttribute("src", "images/back.png");
+		cardToAdd.setAttribute("data-id", i);
+		cardToAdd.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardToAdd);
+	}
+}
+
+createBoard();
