9bf2afdca063106fc635142b76959161f2e8c2e5
canpan14 <sammachlin@hotmail.com>
2017-08-22 07:33:38 -0400

    Preloading cards into cache to improve performance.

--- js/cardsForLevels.js
+++ js/cardsForLevels.js
@@ -62,3 +62,38 @@ function levelThree() {
 	cardsToReturn.push(new card("ace","clubs","images/AC.svg"));
 	return cardsToReturn;
 }
+
+/**
+ * https://stackoverflow.com/questions/10240110/how-do-you-cache-an-image-in-javascript
+ */
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+preloadImages([
+"images/KD.svg",
+"images/KS.svg",
+"images/QH.svg",
+"images/QC.svg",
+"images/JD.svg",
+"images/JH.svg",
+"images/AS.svg",
+"images/AC.svg",
+"images/blue-back.gif"
+]);
